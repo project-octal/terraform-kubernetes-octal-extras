79f4d3bc1091d6f420ead442ba0c9c966ace5db8
Dylan Turnbull <dylanturn@gmail.com>
2021-04-01 11:06:50 -0500

    undo the last change

--- extras-kubedb.tf
+++ extras-kubedb.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_namespace" "kubedb_namespace" {
   # Here we're first checking to see if kubedb was enabled. If enabled we check to see if a custom namespace was specified.
-  count = default(var.enabled_extras["kubedb"]["enabled"],false) ? var.enabled_extras.kubedb.namespace != null ? 1 : 0 : 0
+  count = local.enabled_extras["kubedb"]["enabled"] ? var.enabled_extras.kubedb.namespace != null ? 1 : 0 : 0
 
   metadata {
     name = var.enabled_extras.kubedb.namespace
@@ -9,7 +9,7 @@ resource "kubernetes_namespace" "kubedb_namespace" {
 
 module "octal_extras_kubedb" {
   source = "./terraform-octal-kubedb"
-  count  = default(var.enabled_extras["kubedb"]["enabled"],false) ? 1 : 0
+  count  = local.enabled_extras["kubedb"]["enabled"] ? 1 : 0
 
   argocd_namespace     = var.argocd_namespace
   argocd_project       = module.octal_extras_argocd_project[0].name
--- extras-rookio.tf
+++ extras-rookio.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_namespace" "rookio_namespace" {
   # Here we're first checking to see if rookio was enabled. If enabled we check to see if a custom namespace was specified.
-  count = default(var.enabled_extras["rookio"]["enabled"],false) ? var.enabled_extras.rookio.namespace != null ? 1 : 0 : 0
+  count = local.enabled_extras["rookio"]["enabled"] ? var.enabled_extras.rookio.namespace != null ? 1 : 0 : 0
 
   metadata {
     name = var.enabled_extras.kubedb.namespace
@@ -9,7 +9,7 @@ resource "kubernetes_namespace" "rookio_namespace" {
 
 module "octal_extras_rookio" {
   source = "./terraform-octal-rookio"
-  count  = default(var.enabled_extras["rookio"]["enabled"],false) ? 1 : 0
+  count  = local.enabled_extras["rookio"]["enabled"] ? 1 : 0
 
   argocd_namespace     = var.argocd_namespace
   argocd_project       = module.octal_extras_argocd_project[0].name
