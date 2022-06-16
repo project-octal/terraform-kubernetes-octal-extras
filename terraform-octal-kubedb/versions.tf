361669f7a7a8d605eb56fa6690b86b7eb8bfd224
Dylan Turnbull <dylanturn@gmail.com>
2022-06-16 11:00:58 -0500

    removed sub-module required provider versions

--- terraform-octal-kubedb/versions.tf
+++ terraform-octal-kubedb/versions.tf
@@ -3,7 +3,6 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.6.1"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -4,7 +4,6 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.6.1"
     }
   }
 }
